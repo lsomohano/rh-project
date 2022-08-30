157c6e1b761cc35df01ef45fa4dc948ed72a70c9
lsomohano <lsomohano@avasa.com.mx>
2022-08-30 17:58:47 -0500

    Configuración de template base y pague home

--- reclutamiento/views.py
+++ reclutamiento/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def home(request):
-    titles = {"title_page":'Home',"sub_title_page":'Pagina principal.'}
+    titles = {"title_page":'Dashboard',"sub_title_page":'Pagina principal.'}
 
     return render(request,"reclutamiento/home.html",{"titles":titles})
