a50c2744e0109f0052f42fde4a4ba892417fed9b
lsomohano <lsomohano@avasa.com.mx>
2022-11-17 18:26:20 -0500

    xxxxx

--- facturacion/urls.py
+++ facturacion/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('create_facturacion/',views.createFacturacion, name="CreateFacturacion"),
     #path('edit_solicitudes/<int:id>',views.editSolicitudes, name="EditSolicitudes"),
     path('details_facturacion/<int:id>',views.detailsFacturacion, name="DetailsFacturacion"),
+
+    path('add_candidatos/<fecha_ini>/<fecha_fin>',views.addCandidatos, name="AddCandidatos"),
+    path('candidatos_list/<fecha_ini>/<fecha_fin>',views.listCandidatosJson, name="ListCandidatos"),
 ]
--- facturacion/views.py
+++ facturacion/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .models import Facturas, FacturasCandidatos
 from solicitudes.models import Candidatos
 from .forms import FacturacionForm
+from django.core.serializers import serialize
 
 # Create your views here.
 
@@ -12,7 +14,7 @@ def facturasView(request):
     
     titles = {"title_page":'Facturas',"sub_title_page":'Lista de facturas.'}
     facturas = Facturas.objects.filter(activo='Y')
-    return render(request,"facturacion/facturacion.html",{"titles":titles, "solicitudes":facturas})
+    return render(request,"facturacion/facturacion.html",{"titles":titles, "facturas":facturas})
 
 
 def createFacturacion(request):
@@ -37,21 +39,35 @@ def detailsFacturacion(request, id):
     titles = {"title_page":'Solicitudes',"sub_title_page":'Información de la vacante.'}
     facturacion = Facturas.objects.get(id=id)
 
-    #candidatos = Candidatos.objects.filter(solicitudes_vacantes_id=id).select_related('personas').prefetch_related('id__candidatos_estatus')
-    #candidatos = Candidatos.objects.raw("""SELECT c.id, c.created, c.aceptado, c.personas_id, p.nombre, p.apellido_paterno,p.apellido_materno, p.rfc, p.fecha_nacimiento, p.email, p.genero, ce.created AS fecha_estatus, e.estatus, e.descripcion FROM candidatos c
-                                            #INNER JOIN personas p ON p.id=c.personas_id
-                                            #INNER JOIN candidatos_estatus ce ON ce.candidatos_id=c.id AND ce.activo='Y'
-                                           # INNER JOIN calogos_estatus e ON e.id=ce.estatus_id
-                                            #WHERE c.solicitudes_vacantes_id=%s""",(id,))
+    candidatos = Candidatos.objects.raw(""" SELECT c.id, 
+        p.rfc, p.nombre, p.apellido_paterno, p.apellido_materno, 
+        ce.created AS estatus_fecha, ec.estatus, 
+        u.first_name, u.last_name, 
+        pr.rfc AS rfc_proveedor, 
+        s.id AS solicitudes_id, 
+        l.locacion, 
+        po.puesto_operativo
+        FROM candidatos c
+        INNER JOIN personas p ON p.id=c.personas_id
+        INNER JOIN candidatos_estatus ce ON ce.candidatos_id=c.id AND ce.activo='Y'
+        INNER JOIN calogos_estatus ec ON ec.id=ce.estatus_id
+        INNER JOIN autenticacion_user u ON u.id=c.user_id
+        INNER JOIN contactos_proveedores cp ON cp.user_id=u.id
+        INNER JOIN proveedores pr ON pr.id=cp.proveedores_id 
+        INNER JOIN solicitudes_vacantes s ON s.id=c.solicitudes_vacantes_id
+        INNER JOIN locaciones l ON l.id=s.locaciones_id
+        INNER JOIN puestos_operativos po ON po.id=s.puestos_operativos_id
+        WHERE ec.estatus='Contratado' AND ce.created BETWEEN %s AND %s """,(facturacion.fecha_ini, facturacion.fecha_fin,))
                                                 
 
     return render(request,"facturacion/details_facturacion.html",{
         "titles":titles, 
-        "facturacion":facturacion
+        "facturacion":facturacion,
+        "candidatos":candidatos,
     })
 
 
-def createFacturasCandidatos(request):
+def addCandidatos(request,fecha_ini,fecha_fin):
     """Vista que permite agregar nuevas solicitudes"""
 
     titles = {"title_page":'Faturación',"sub_title_page":'Agregar candidatos al reporte.'}
@@ -61,7 +77,51 @@ def createFacturasCandidatos(request):
             facturacion = formulario.save()
             return redirect('DetailsFacturacion', id=facturacion.id)
     
-    candidatos = Cand
+    candidatos = Candidatos.objects.raw(""" SELECT c.id, 
+        p.rfc, p.nombre, p.apellido_paterno, p.apellido_materno, 
+        ce.created AS estatus_fecha, ec.estatus, 
+        u.first_name, u.last_name, 
+        pr.rfc as rfc_proveedor, 
+        s.id AS solicitudes_id, 
+        l.locacion, 
+        po.puesto_operativo
+        FROM candidatos c
+        INNER JOIN personas p ON p.id=c.personas_id
+        INNER JOIN candidatos_estatus ce ON ce.candidatos_id=c.id AND ce.activo='Y'
+        INNER JOIN calogos_estatus ec ON ec.id=ce.estatus_id
+        INNER JOIN autenticacion_user u ON u.id=c.user_id
+        INNER JOIN contactos_proveedores cp ON cp.user_id=u.id
+        INNER JOIN proveedores pr ON pr.id=cp.proveedores_id 
+        INNER JOIN solicitudes_vacantes s ON s.id=c.solicitudes_vacantes_id
+        INNER JOIN locaciones l ON l.id=s.locaciones_id
+        INNER JOIN puestos_operativos po ON po.id=s.puestos_operativos_id
+        WHERE ec.estatus='Contratado' AND ce.created BETWEEN %s AND %s """,(fecha_ini, fecha_fin,))
+        
     formulario = FacturacionForm()
 
-    return render(request,"facturacion/create_facturacion.html",{"titles":titles, "formulario":formulario})
+    return render(request,"facturacion/add_candidatos.html",{"titles":titles, "candidatos":candidatos})
+
+
+def listCandidatosJson(request,fecha_ini,fecha_fin):
+    
+    candidatos = serialize('json', Candidatos.objects.raw(""" SELECT c.id, 
+        p.rfc, p.nombre, p.apellido_paterno, p.apellido_materno, 
+        ce.created AS estatus_fecha, ec.estatus, 
+        u.first_name, u.last_name, 
+        pr.rfc as rfc_proveedor, 
+        s.id AS solicitudes_id, 
+        l.locacion, 
+        po.puesto_operativo
+        FROM candidatos c
+        INNER JOIN personas p ON p.id=c.personas_id
+        INNER JOIN candidatos_estatus ce ON ce.candidatos_id=c.id AND ce.activo='Y'
+        INNER JOIN calogos_estatus ec ON ec.id=ce.estatus_id
+        INNER JOIN autenticacion_user u ON u.id=c.user_id
+        INNER JOIN contactos_proveedores cp ON cp.user_id=u.id
+        INNER JOIN proveedores pr ON pr.id=cp.proveedores_id 
+        INNER JOIN solicitudes_vacantes s ON s.id=c.solicitudes_vacantes_id
+        INNER JOIN locaciones l ON l.id=s.locaciones_id
+        INNER JOIN puestos_operativos po ON po.id=s.puestos_operativos_id
+        WHERE ec.estatus='Contratado' AND ce.created BETWEEN %s AND %s """,(fecha_ini, fecha_fin,)))
+
+    return HttpResponse(candidatos,'application/json')
